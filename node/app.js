3c2dfed15cb241e4e2e5a26dcafa4c1df1406dac
larry kirschner <larrykirschner@gmail.com>
2019-11-20 15:59:46 -0800

    fixes build errors

--- node/app.js
+++ node/app.js
@@ -10,6 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", express.static(path.join(__dirname, "public", "mentorpanel")));
+app.use("/config", (req, res, next) => {
+  res.send({
+    MENTOR_API_URL: process.env.MENTOR_API_URL || "/mentor-api",
+    MENTOR_VIDEO_URL: process.env.MENTOR_VIDEO_URL || "/"
+  });
+});
 const port = process.env.NODE_PORT || 3000;
 server.listen(port, function() {
   console.log(`node listening on port ${port}`);
