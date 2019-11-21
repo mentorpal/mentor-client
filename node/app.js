12673de70fd507485cebece73a6d0e577f3942a9
larry kirschner <larrykirschner@gmail.com>
2019-11-20 17:15:30 -0800

    rsync

--- node/app.js
+++ node/app.js
@@ -8,22 +8,22 @@ var server = http.createServer(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/", express.static(path.join(__dirname, "public", "mentorpanel")));
 app.use(cors());
+app.use("/", express.static(path.join(__dirname, "public", "mentorpanel")));
 app.get("/config", (req, res, next) => {
   res.send({
     MENTOR_API_URL: process.env.MENTOR_API_URL || "/mentor-api",
     MENTOR_VIDEO_URL: process.env.MENTOR_VIDEO_URL || "/videos",
   });
 });
-app.get(/mentor-api\/*/, cors(), (req, res, next) => {
+app.get(/mentor-api\/*/, (req, res, next) => {
   // expectation is that MENTOR_VIDEO_URL env is configured
   res.redirect(
     301,
     process.env.MENTOR_API_URL + req.url.replace(/^\/mentor-api/, "")
   );
 });
-app.get(/videos\/*/, cors(), (req, res, next) => {
+app.get(/videos\/*/, (req, res, next) => {
   // expectation is that MENTOR_VIDEO_URL env is configured
   res.redirect(
     301,
