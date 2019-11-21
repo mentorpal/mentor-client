ccf1bf9af96eec0d039567406ae044bdebdee1d1
larry kirschner <larrykirschner@gmail.com>
2019-11-20 16:23:13 -0800

    get configured mentorapi and host

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosResponse } from "axios";
 
-const MENTOR_API_URL = process.env.MENTOR_API_URL || "/mentor-api"; // eslint-disable-line no-undef
-let MENTOR_VIDEO_HOST =
-  process.env.MENTOR_VIDEO_HOST || "https://video.mentorpal.org";
+let MENTOR_API_URL = process.env.MENTOR_API_URL || "/mentor-api"; // eslint-disable-line no-undef
+let MENTOR_VIDEO_URL =
+  process.env.MENTOR_VIDEO_URL || "/videos";
 
 export interface MentorApiData {
   id: string;
@@ -47,7 +47,7 @@ if (typeof window !== "undefined" && process.env.NODE_ENV !== "test") {
     .then(result => {
       console.log(`get ${MENTOR_API_URL}/config/video-host`, result);
       if (typeof result.data.url === "string") {
-        MENTOR_VIDEO_HOST = result.data.url;
+        MENTOR_VIDEO_URL = result.data.url;
       }
     })
     .catch(err => {
@@ -60,11 +60,11 @@ export const videoUrl = (
   answerId: string,
   format: string
 ): string => {
-  return `${MENTOR_VIDEO_HOST}/videos/mentors/${mentor}/${format}/${answerId}.mp4`;
+  return `${MENTOR_VIDEO_URL}/mentors/${mentor}/${format}/${answerId}.mp4`;
 };
 
 export const idleUrl = (mentor: string, format: string): string => {
-  return `${MENTOR_VIDEO_HOST}/videos/mentors/${mentor}/${format}/idle.mp4`;
+  return `${MENTOR_VIDEO_URL}/mentors/${mentor}/${format}/idle.mp4`;
 };
 
 // TODO: don't pass mentor here, pass mentorId and answerId
@@ -89,15 +89,4 @@ export const queryMentor = async (
       query: question,
     },
   });
-  // const { data } = res;
-  // const response = {
-  //   id: mentorId,
-  //   question: data.query,
-  //   answer_id: data.answer_id,
-  //   answer_text: data.answer_text,
-  //   confidence: data.confidence,
-  //   is_off_topic: data.confidence <= RESPONSE_CUTOFF,
-  //   classifier: data.classifier,
-  // };
-  // return response;
 };
--- node/app.js
+++ node/app.js
@@ -10,11 +10,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", express.static(path.join(__dirname, "public", "mentorpanel")));
-app.use("/config", (req, res, next) => {
+app.get("/config", (req, res, next) => {
   res.send({
     MENTOR_API_URL: process.env.MENTOR_API_URL || "/mentor-api",
-    MENTOR_VIDEO_URL: process.env.MENTOR_VIDEO_URL || "/"
+    MENTOR_VIDEO_URL: process.env.MENTOR_VIDEO_URL || "/videos"
   });
+});
+app.get(/mentor-api\/*/, (req, res, next) => {
+  // expectation is that MENTOR_VIDEO_URL env is configured
+  res.redirect(
+    301,
+    process.env.MENTOR_API_URL + req.url.replace(/^\/mentor-api/, "")
+  );
+});
+app.get(/videos\/*/, (req, res, next) => {
+  // expectation is that MENTOR_VIDEO_URL env is configured
+  res.redirect(
+    301,
+    process.env.MENTOR_VIDEO_URL + req.url.replace(/^\/videos/, "")
+  );
 });
 const port = process.env.NODE_PORT || 3000;
 server.listen(port, function() {
