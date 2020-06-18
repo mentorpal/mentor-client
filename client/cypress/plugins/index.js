e5b6510b506077e5813c1a31a931e84ba4275c8f
Martin Acob <macob@LMAC40.local>
2020-06-18 10:20:38 -0700

    Adding visual testing.

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -21,3 +21,11 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
+
+//cypress-image-snapshot
+const {
+  addMatchImageSnapshotPlugin,
+ } = require('cypress-image-snapshot/plugin');
+ module.exports = (on, config) => {
+   addMatchImageSnapshotPlugin(on, config);
+ };
--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -29,3 +29,19 @@ The full terms of this copyright and license should always be found in the root 
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+//cypress-image-snapshot
+import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+addMatchImageSnapshotCommand({
+  failureThreshold: 0.008,
+  failureThresholdType: 'percent',
+  customDiffConfig: { threshold: 0.0 },
+  capture: 'viewport',
+});
+Cypress.Commands.add("setResolution", (size) => {
+  if (Cypress._.isArray(size)) {
+     cy.viewport(size[0], size[1]);
+   } else {
+    cy.viewport(size);
+  }
+ })
