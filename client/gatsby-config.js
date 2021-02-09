073ad4461cedd72735ec18cfa0f60ad8912e3ed5
kycarr <whizbon@gmail.com>
2021-02-09 12:35:27 -0800

    Update react-player to official latest version (#25)

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,47 +1,47 @@
 module.exports = {
-    pathPrefix: `/client`,
-    siteMetadata: {
-        title: `MentorPanel`,
-        description: ``,
-        author: `@gatsbyjs`,
+  pathPrefix: `/client`,
+  siteMetadata: {
+    title: `MentorPanel`,
+    description: ``,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-typescript`,
+    {
+      resolve: "gatsby-plugin-material-ui",
     },
-    plugins: [
-        `gatsby-plugin-typescript`,
-        {
-            resolve: "gatsby-plugin-material-ui",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-env-variables`,
+      options: {
+        whitelist: ["CMI5_ENDPOINT"],
+        whitelist: ["CMI5_FETCH"],
+        whitelist: ["MENTOR_API_URL"],
+        whitelist: ["MENTOR_VIDEO_URL"],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public|static)/,
+        stages: ["develop"],
+        options: {
+          emitWarning: true,
+          failOnError: false,
         },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/icon.png`,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-env-variables`,
-            options: {
-                whitelist: ["CMI5_ENDPOINT"],
-                whitelist: ["CMI5_FETCH"],
-                whitelist: ["MENTOR_API_URL"],
-                whitelist: ["MENTOR_VIDEO_URL"],
-            },
-        },
-        {
-            resolve: "gatsby-plugin-eslint",
-            options: {
-                test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
-                exclude: /(node_modules|.cache|public|static)/,
-                stages: ["develop"],
-                options: {
-                    emitWarning: true,
-                    failOnError: false,
-                },
-            },
-        },
-    ],
+      },
+    },
+  ],
 };
