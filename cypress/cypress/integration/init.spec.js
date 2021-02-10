293486a7743b71216c1c0b51530bb74cdbfac33f
Kayla Carr <whizbon@gmail.com>
2021-02-10 11:14:29 -0800

    Remove localhost:8000 test from init

--- cypress/cypress/integration/init.spec.js
+++ cypress/cypress/integration/init.spec.js
@@ -11,10 +11,6 @@ describe("Cypress", () => {
         expect(true).to.equal(true);
     });
 
-    it("visits the app at localhost:8000", () => {
-        cy.visit("http://localhost:8000", { qs: addGuestParams() });
-    });
-
     it("visits the app at base URL", () => {
         cy.visit("/", { qs: addGuestParams() });
     });
