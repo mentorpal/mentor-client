7f3344171c53126260ed8cf82010e615280272da
larry kirschner <larrykirschner@gmail.com>
2021-04-07 13:30:41 -0700

    wip: get rid of https redirect

--- client/wrap-with-provider.jsx
+++ client/wrap-with-provider.jsx
@@ -6,7 +6,7 @@ import thunk from "redux-thunk";
 import store from "store/reducer";
 import logger from "redux-logger";
 
-const storeObj = createStore(store, applyMiddleware(...[thunk]));
+const storeObj = createStore(store, applyMiddleware(...[thunk, logger]));
 
 export default function WrappedWithProvider({ element }) {
   return <Provider store={storeObj}>{element}</Provider>;
