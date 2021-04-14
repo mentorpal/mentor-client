4d8f432798473e8f5211f9ef06f3be8a0337bd8e
larry kirschner <larrykirschner@gmail.com>
2021-04-14 13:45:23 -0700

    removes PWA support and favicon

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -3,22 +3,9 @@ module.exports = {
   siteMetadata: {
     title: `Chat`,
     description: ``,
-    author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: "src/images/icon.png",
-      },
-    },
     {
       resolve: `gatsby-plugin-env-variables`,
       options: {
